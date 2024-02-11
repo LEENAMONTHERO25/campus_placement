9fdac12eece04842ecc0f642401c38fa2bb0c238
Leena Monthero <leenamonthero@gmail.com>
2024-02-11 16:49:21 +0900

    code updated

--- src/Campus_Placement/components/data_ingestion.py
+++ src/Campus_Placement/components/data_ingestion.py
@@ -1,62 +1,56 @@
+import pandas as pd
+import numpy as np
+from src.Campus_Placement.logger import logging
+from src.Campus_Placement.exception import customexception
+
 import os
 import sys
-from src.exception import CustomException
-from src.logger import logging
-import pandas as pd
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from pathlib import Path
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+class DataIngestionConfig:
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
-
-@dataclass     # using this we can define variables directly without init
-class DataIngestionConfig:              #we create this class for where we save test,train,raw data these kind of information
-    train_data_path: str=os.path.join('artifacts',"train.csv")  # all output files save here
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()  # here above three paths saved in this object
-
+        self.ingestion_config=DataIngestionConfig()
+        
+    
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
+        logging.info("data ingestion started")
+        
         try:
-            df=pd.read_csv('D:\campus_placement\notebooks\data\placement_train.csv')
-            logging.info('Read the dataset as dataframe')
-
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)   # here we create artifact folder,if folder is already there we dont want to delete it ..so we write exist_ok
-
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)  # this is for raw data
-
-            logging.info("Train test split initiated")
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
-
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) # here we split the data and save it in artifacts
-
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-            logging.info("Ingestion of the data is completed")
-
-            return(
+            data=pd.read_csv(Path(os.path.join("D:\campus_placement\notebooks\data\placement_train.csv")))
+            logging.info(" i have read dataset as a df")
+            
+            
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info(" i have saved the raw dataset in artifact folder")
+            
+            logging.info("here i have performed train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion part completed")
+            
+            return (
+                 
+                
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-
             )
+            
+            
         except Exception as e:
-            raise CustomException(e,sys)
-        
-if __name__=="__main__":        # here we initiate and run it
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_ =data_transformation.initiate_data_transformation(train_data,test_data)
-
-
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+           logging.info("exception during occured at data ingestion stage")
+           raise customexception(e,sys)
+    
--- src/Campus_Placement/exception.py
+++ src/Campus_Placement/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from src.Campus_Placement.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -12,7 +12,7 @@ def error_message_detail(error,error_detail:sys):
     
 
 
-class CustomException(Exception):
+class customexception(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     
     except Exception as e:
         logging.info("Divide by zero")
-        raise CustomException(e,sys)
+        raise customexception(e,sys)
         
